0e7d34e2a0d18a3481c506743482fd914c70db2e
Rusan Ahsan <rusanpubg@gmail.com>
2022-01-26 16:50:53 +0530

    cors added

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ const latlngRouter=require('./routes/latlng');
 const notFoundMiddleware=require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const authenticateUser = require('./middleware/authentication');
+const cors=require('cors');
 
+app.use(cors());
 app.use(express.static('./public'));
 app.use(express.json());
-
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/reviews', authenticateUser,reviewsRouter);
 app.use('/api/v1/latlng',authenticateUser,latlngRouter);
